7de0f78dba09f63d3ca67bedba2c54b98293ed6c
hsalmeida <hsalmeida@gmail.com>
2016-11-11 14:08:13 -0200

    responsive data table

--- js/components/workspace.js
+++ js/components/workspace.js
@@ -73,6 +73,7 @@ angular.module('team-task')
         function ($scope, $rootScope, $state, Atividade, Time, DTOptionsBuilder, $resource, $filter, Pessoa) {
             $scope.showLoading = false;
             $scope.dtAOptions = DTOptionsBuilder.newOptions().withLanguage($resource('js/dtOptions.json').get().$promise);
+            $scope.dtAOptions.withOption('responsive', true);
 
             function loadTable() {
                 $scope.listaAtividades = [];
