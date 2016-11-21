8b657e68a8593dbe27ef3e18eab5f16d57f96478
hsalmeida <hsalmeida@gmail.com>
2016-11-21 15:11:54 -0200

    inicio da implementacao do workforce.

--- js/components/workspace.js
+++ js/components/workspace.js
@@ -210,7 +210,8 @@ angular.module('team-task')
                                             row.tasks.push({
                                                 "name": atividades[indexTimeAtividade].nome,
                                                 "from": moment(atividades[indexTimeAtividade].inicio.$date),
-                                                "to": moment(atividades[indexTimeAtividade].fim.$date)
+                                                "to": moment(atividades[indexTimeAtividade].fim.$date),
+                                                "color": "#F1C232"
                                             });
                                         }
 
@@ -227,7 +228,8 @@ angular.module('team-task')
                                                         row.tasks.push({
                                                             "name": projetos[p].atividades[at].nome,
                                                             "from": moment(projetos[p].atividades[at].inicio.$date),
-                                                            "to": moment(projetos[p].atividades[at].fim.$date)
+                                                            "to": moment(projetos[p].atividades[at].fim.$date),
+                                                            "color": "#F1C232"
                                                         });
                                                     }
                                                 }
@@ -240,149 +242,49 @@ angular.module('team-task')
                         });
                     }
                 });
-
-
-                /*$scope.ganttData = [
-                    {
-                        name: 'Create concept', tasks: [
-                        {
-                            name: 'Create concept',
-                            content: '<i class="fa fa-cog" ng-click="scope.handleTaskIconClick(task.model)"></i> {{task.model.name}}',
-                            color: '#F1C232',
-                            from: new Date(2013, 9, 10, 8, 0, 0),
-                            to: new Date(2013, 9, 16, 18, 0, 0),
-                            est: new Date(2013, 9, 8, 8, 0, 0),
-                            lct: new Date(2013, 9, 18, 20, 0, 0),
-                            progress: 100
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Finalize concept', tasks: [
-                        {
-                            name: 'Finalize concept',
-                            color: '#F1C232',
-                            from: new Date(2013, 9, 17, 8, 0, 0),
-                            to: new Date(2013, 9, 18, 18, 0, 0),
-                            progress: 100
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Development',
-                        children: ['Sprint 1', 'Sprint 2', 'Sprint 3', 'Sprint 4'],
-                        content: '<i class="fa fa-file-code-o" ng-click="scope.handleRowIconClick(row.model)"></i> {{row.model.name}}'
-                    },
-                    {
-                        name: 'Sprint 1', tooltips: false, tasks: [
-                        {
-                            name: 'Product list view',
-                            color: '#F1C232',
-                            from: new Date(2013, 9, 21, 8, 0, 0),
-                            to: new Date(2013, 9, 25, 15, 0, 0),
-                            progress: 25
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Sprint 2', tasks: [
-                        {
-                            name: 'Order basket',
-                            color: '#F1C232',
-                            from: new Date(2013, 9, 28, 8, 0, 0),
-                            to: new Date(2013, 10, 1, 15, 0, 0)
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Sprint 3', tasks: [
-                        {
-                            name: 'Checkout',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 4, 8, 0, 0),
-                            to: new Date(2013, 10, 8, 15, 0, 0)
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Sprint 4', tasks: [
-                        {
-                            name: 'Login & Signup & Admin Views',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 11, 8, 0, 0),
-                            to: new Date(2013, 10, 15, 15, 0, 0)
-                        }
-                    ]
-                    },
-                    {name: 'Hosting'},
-                    {
-                        name: 'Setup', tasks: [
-                        {
-                            name: 'HW',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 18, 8, 0, 0),
-                            to: new Date(2013, 10, 18, 12, 0, 0)
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Config', tasks: [
-                        {
-                            name: 'SW / DNS/ Backups',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 18, 12, 0, 0),
-                            to: new Date(2013, 10, 21, 18, 0, 0)
-                        }
-                    ]
-                    },
-                    {name: 'Server', parent: 'Hosting', children: ['Setup', 'Config']},
-                    {
-                        name: 'Deployment', parent: 'Hosting', tasks: [
-                        {
-                            name: 'Depl. & Final testing',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 21, 8, 0, 0),
-                            to: new Date(2013, 10, 22, 12, 0, 0),
-                            'classes': 'gantt-task-deployment'
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Workshop', tasks: [
-                        {
-                            name: 'On-side education',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 24, 9, 0, 0),
-                            to: new Date(2013, 10, 25, 15, 0, 0)
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Content', tasks: [
-                        {
-                            name: 'Supervise content creation',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 26, 9, 0, 0),
-                            to: new Date(2013, 10, 29, 16, 0, 0)
-                        }
-                    ]
-                    },
-                    {
-                        name: 'Documentation', tasks: [
-                        {
-                            name: 'Technical/User documentation',
-                            color: '#F1C232',
-                            from: new Date(2013, 10, 26, 8, 0, 0),
-                            to: new Date(2013, 10, 28, 18, 0, 0)
-                        }
-                    ]
-                    }
-                ];
-                */
                 $scope.showLoading = false;
             }
 
+            $scope.canAutoWidth = function(scale) {
+                if (scale.match(/.*?hour.*?/) || scale.match(/.*?minute.*?/)) {
+                    return false;
+                }
+                return true;
+            };
+
+            $scope.getColumnWidth = function(widthEnabled, scale, zoom) {
+                if (!widthEnabled && $scope.canAutoWidth(scale)) {
+                    return undefined;
+                }
+
+                if (scale.match(/.*?week.*?/)) {
+                    return 150 * zoom;
+                }
+
+                if (scale.match(/.*?month.*?/)) {
+                    return 300 * zoom;
+                }
+
+                if (scale.match(/.*?quarter.*?/)) {
+                    return 500 * zoom;
+                }
+
+                if (scale.match(/.*?year.*?/)) {
+                    return 800 * zoom;
+                }
+                return 40 * zoom;
+            };
+
             $scope.initWorkspaceWorkforce = function () {
+
+
+
+                $scope.dateFormat = "dddd, DD/MM/YYYY";
+                $scope.ganttOptions = {
+                    "zoom": 1,
+                    "scale": "day",
+                    "width": true
+                };
                 loadTable();
             };
         }]);
