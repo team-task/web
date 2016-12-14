5d54190906adb921364d1fa2d7a4d7be5b0ab71c
hsalmeida <hsalmeida@gmail.com>
2016-12-14 17:45:54 -0200

    filtro de status

--- js/directives.js
+++ js/directives.js
@@ -1,4 +1,20 @@
 angular.module('team-task')
+    .directive(
+    "mAppLoading",
+    function ($animate) {
+        return ({
+            link: link,
+            restrict: "C"
+        });
+        function link(scope, element, attributes) {
+            $animate.leave(element.children().eq(1)).then(
+                function cleanupAfterAnimation() {
+                    element.remove();
+                    scope = element = attributes = null;
+                }
+            );
+        }
+    })
     .directive('header', function () {
         return {
             restrict: 'A',
