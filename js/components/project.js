14de4fb845fab3520a35ec22e6798d27fba0f0ff
hsalmeida <hsalmeida@gmail.com>
2016-12-19 15:13:25 -0200

    gantt no projeto

--- js/components/project.js
+++ js/components/project.js
@@ -43,6 +43,26 @@ angular.module('team-task')
             $scope.initWorkspaceProject = function () {
                 $scope.filtro = [true, true, false, false];
                 $scope.listaFiltro= ["aguardando", "iniciada", "concluída", "cancelada"];
+                $scope.ganttData = [];
+                $scope.ganttOptions= {
+                    "zoom": 1,
+                    "scale": "week",
+                    "width": true,
+                    "currentDate": 'line',
+                    "tableHeaders": {'model.name': 'Time / Atividade'},
+                    "daily": true,
+                    "sortMode": ["model.time.nome", "model.atividade.nome", "from"],
+                    "contents": {
+                        'model.name': '<a ng-click="scope.mostrarDetalheAtividadeGantt(row.model)" class="pointer-action">{{getValue()}}</a>' +
+                        '&nbsp;<span class="pointer-action fa fa-pencil-square-o"' +
+                        'ng-click="scope.editarAtividadeGantt(row.model)">' +
+                        '</span>'
+                    },
+                    "filtertask": ["aguardando", "iniciada", "concluída"],
+                    api: function (api) {
+                        $scope.api = api;
+                    }
+                };
                 loadProject();
             };
 
@@ -52,10 +72,11 @@ angular.module('team-task')
 
             function loadProject() {
                 $scope.showLoading = true;
-
+                $scope.ganttData = [];
                 Projeto.getById($stateParams.id).then(function (projeto) {
                     if (projeto) {
-                        angular.forEach(projeto.atividades, function (atividade, idAtividade) {
+                        var prom = [];
+                        prom.push(angular.forEach(projeto.atividades, function (atividade, idAtividade) {
                             if (atividade.designado) {
                                 Pessoa.getById(atividade.designado).then(function (pessoa) {
                                     if (pessoa) {
@@ -87,9 +108,36 @@ angular.module('team-task')
                                 "Inicio": "",
                                 "Duracao": ""
                             });
+                        }));
+
+                        $q.all(prom).then(function () {
+                            for (var at = 0; at < projeto.atividades.length; at++) {
+                                var atividade = projeto.atividades[at];
+                                var rowPr = {
+                                    "name": atividade.nomeTime + " / " + atividade.nome,
+                                    "atividade": atividade.nome,
+                                    "projeto": projeto,
+                                    "indiceAt": at
+                                };
+                                rowPr.tasks = [];
+                                var listDep = [];
+                                if(atividade.predecessora) {
+                                    listDep.push({"from": atividade.predecessora.nomeComposto});
+                                }
+                                rowPr.tasks.push({
+                                    "id": projeto.nome + " / " + atividade.nome,
+                                    "name": atividade.nome,
+                                    "from": moment(atividade.inicio.$date),
+                                    "to": moment(atividade.fim.$date),
+                                    "color": "#9FC5F8",
+                                    "status": atividade.status,
+                                    "dependencies": listDep
+                                });
+                                $scope.ganttData.push(rowPr);
+                            }
+                            $scope.projeto = projeto;
+                            $scope.showLoading = false;
                         });
-                        $scope.projeto = projeto;
-                        $scope.showLoading = false;
                     }
                 });
             }
@@ -188,4 +236,44 @@ angular.module('team-task')
                     }, function () {
                     });
             };
+
+
+            $scope.editarAtividadeGantt = function (model) {
+                if(model.projeto) {
+                    $uibModal
+                        .open({
+                            templateUrl: 'views/modal/edit-activity.html',
+                            controller: 'ModalEditActivityController',
+                            resolve: {
+                                projetoSelecionado: function () {
+                                    return model.projeto;
+                                },
+                                indice: function () {
+                                    return model.indiceAt;
+                                }
+                            }
+                        }).result.then(function () {
+                            loadProject();
+                            $rootScope.$emit("CallLoadMenus", {});
+                        }, function () {
+                        });
+                }
+            };
+
+            $scope.mostrarDetalheAtividadeGantt = function (model) {
+                $uibModal
+                    .open({
+                        templateUrl: 'views/modal/view-activity.html',
+                        controller: 'ModalViewActivityController',
+                        resolve: {
+                            projetoSelecionado: function () {
+                                return model.projeto;
+                            },
+                            indice: function () {
+                                return model.indiceAt;
+                            }
+                        }
+                    }).result.then(function () {}, function () {});
+            };
+
         }]);
