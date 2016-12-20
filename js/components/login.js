1e92be4f19776c911404e5cb7223c8a537abb6ee
hsalmeida <hsalmeida@gmail.com>
2016-12-20 14:36:20 -0200

    fabrica de login

--- js/components/login.js
+++ js/components/login.js
@@ -1,6 +1,6 @@
 angular.module('team-task')
-    .controller('LoginController', ['$scope', '$rootScope', '$state', 'Pessoa', '$window',
-        function ($scope, $rootScope, $state, Pessoa, $window) {
+    .controller('LoginController', ['$scope', '$rootScope', '$state', 'Pessoa', '$window', 'LoginFactory',
+        function ($scope, $rootScope, $state, Pessoa, $window, LoginFactory) {
             var vm = this;
             vm.errorLogin = "";
             vm.usuario = "";
@@ -14,31 +14,6 @@ angular.module('team-task')
             $scope.login = function () {
                 var usuario = vm.usuario;
                 var senha = vm.senha;
-                waitingDialog.show("Validando login na rede. Aguarde");
-                var query = {
-                    "usuario": usuario,
-                    "senha": senha,
-                    "ativo": true
-                };
-                Pessoa.query(query).then(function (pessoas) {
-                    if (pessoas[0]) {
-                        waitingDialog.hide();
-                        var logado = pessoas[0];
-                        var nomes = logado.nome.split(" ");
-                        var iniciais = nomes[0].substring(0, 1);
-                        var nomeSimples = nomes[0];
-                        if (nomes.length > 1) {
-                            iniciais += nomes[1].substring(0, 1);
-                        }
-                        logado.iniciais = iniciais.toUpperCase();
-                        logado.nomeSimples = nomeSimples;
-                        $window.sessionStorage.setItem('usuarioLogado', angular.toJson(pessoas[0]));
-
-                        $state.go('workspace-projects');
-                    } else {
-                        vm.errorLogin = "Login e ou senha inválidos";
-                        waitingDialog.hide();
-                    }
-                });
+                LoginFactory.login(usuario, senha, vm);
             };
         }]);
--- js/services.js
+++ js/services.js
@@ -1,5 +1,4 @@
 angular.module('team-task')
-
 	.factory('Atividade', function ($mongolabResourceHttp) {
 		return $mongolabResourceHttp('atividade');
 	})
@@ -11,4 +10,34 @@ angular.module('team-task')
 	})
 	.factory('Time', function ($mongolabResourceHttp) {
 		return $mongolabResourceHttp('time');
+	})
+	.factory('LoginFactory', function LoginFactory (Pessoa, $window, $state) {
+		LoginFactory.login = function (usuario, senha, vm) {
+			waitingDialog.show("Validando login na rede. Aguarde");
+			var query = {
+				"usuario": usuario,
+				"senha": senha,
+				"ativo": true
+			};
+			Pessoa.query(query).then(function (pessoas) {
+				if (pessoas[0]) {
+					waitingDialog.hide();
+					var logado = pessoas[0];
+					var nomes = logado.nome.split(" ");
+					var iniciais = nomes[0].substring(0, 1);
+					var nomeSimples = nomes[0];
+					if (nomes.length > 1) {
+						iniciais += nomes[1].substring(0, 1);
+					}
+					logado.iniciais = iniciais.toUpperCase();
+					logado.nomeSimples = nomeSimples;
+					$window.sessionStorage.setItem('usuarioLogado', angular.toJson(pessoas[0]));
+					$state.go('workspace-projects');
+				} else {
+					vm.errorLogin = "Login e ou senha inválidos";
+					waitingDialog.hide();
+				}
+			});
+		};
+		return LoginFactory;
 	});
