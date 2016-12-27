eda8cf21fb8b54fae1eb09b65181fccf6ad8907f
hsalmeida <hsalmeida@gmail.com>
2016-12-27 16:45:46 -0200

    filtro de status de projetos

--- js/components/workspace-projects.js
+++ js/components/workspace-projects.js
@@ -1,17 +1,20 @@
 angular.module('team-task')
     .controller('WorkspaceProjectsController', ['$scope', '$rootScope', '$state', 'Projeto', 'Atividade', 'Time',
-        'DTOptionsBuilder', '$resource', '$uibModal',
-        function ($scope, $rootScope, $state, Projeto, Atividade, Time, DTOptionsBuilder, $resource, $uibModal) {
+        'DTOptionsBuilder', '$resource', '$uibModal', '$filter',
+        function ($scope, $rootScope, $state, Projeto, Atividade, Time, DTOptionsBuilder, $resource, $uibModal, $filter) {
             $scope.showLoading = false;
             //$scope.dtOptions = DTOptionsBuilder.newOptions().withLanguage($resource('js/dtOptions.json').get().$promise);
 
             $scope.initWorkspaceProjects = function () {
+                $scope.filtro = [true, true, false, false];
+                $scope.listaFiltro= ["Ativo", "Suspenso", "Concluído", "Cancelado"];
                 loadTable();
             };
 
             function loadTable() {
                 $scope.showLoading = true;
                 $scope.listaProjetos = [];
+                $scope.listaProjetosRoot = [];
                 if($rootScope.usuarioLogado) {
                     var idusuario = $rootScope.usuarioLogado._id.$oid;
                     var qTime = {
@@ -34,10 +37,47 @@ angular.module('team-task')
                             };
                             Projeto.query(pQuery).then(function (projetos) {
                                 $scope.listaProjetos = projetos;
+                                $scope.listaProjetosRoot = projetos;
+                                $scope.filterChange();
                                 $scope.showLoading = false;
                             });
                         }
                     });
+                }
+            }
+
+            $scope.filtro = [];
+            $scope.filterChange = function () {
+                if($scope.filtro) {
+                    var listaFiltro = [];
+                    if ($scope.filtro[0]) {
+                        listaFiltro.push("Ativo");
+                    }
+                    if ($scope.filtro[1]) {
+                        listaFiltro.push("Suspenso");
+                    }
+                    if ($scope.filtro[2]) {
+                        listaFiltro.push("Concluído");
+                    }
+                    if ($scope.filtro[3]) {
+                        listaFiltro.push("Cancelado");
+                    }
+                    if(listaFiltro.length > 0) {
+                        $scope.listaFiltro = listaFiltro;
+                        $scope.listaProjetos = $filter('filter')($scope.listaProjetosRoot, filterFunction);
+                    } else {
+                        $scope.listaProjetos = [];
+                    }
+                }
+            };
+
+            function filterFunction (item) {
+                if(item) {
+                    if($scope.listaFiltro) {
+                        return $scope.listaFiltro.indexOf(item.status) > -1;
+                    } else {
+                        return false;
+                    }
                 }
             }
 
